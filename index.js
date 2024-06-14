fdedd40920ed7516698729f1b288d05fbde857f8
AUDACITY <STEPHENSAPPOR5@GMAIL.COM>
2024-06-14 11:12:07 +0000

    Project

---
+++ index.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
